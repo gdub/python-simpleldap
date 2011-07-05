31d8dde5adedd6c89ea1f15bc81e9a783379e17f
Gary Wilson Jr. <gary.wilson@gmail.com>
2011-07-05 13:59:21 -0500

    In setup.py, changed name and added keywords, license, and install dependencies.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ use_setuptools()
 from setuptools import setup
 
 setup(
-    name="simpleldap",
+    name="python-simpleldap",
     version="0.7",
     description="A module that makes simple LDAP usage simple.",
+    keywords="ldap simple simpleldap",
     author="Gary Wilson Jr.",
     author_email="gary.wilson@gmail.com",
     url="https://bitbucket.org/gdub/python-simpleldap/",
+    license="MIT",
     py_modules=["simpleldap"],
+    install_requires=["python-ldap"],
     use_2to3=True,
 )
